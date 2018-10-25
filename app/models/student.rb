28ccc78c8b4820348baf3f7863f9934266eeb5a1
Alexandra Fren <alexandra.fren@gmail.com>
2018-10-25 20:01:21 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,6 +15,6 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def search(student)
-    @student = Student.find_by(name: student)
+    Student.find_by(name: student)
   end
 end

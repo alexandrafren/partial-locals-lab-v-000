6183a19968244fdf78db1f58989992b0d573a865
Alexandra Fren <alexandra.fren@gmail.com>
2018-10-25 20:10:56 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,7 +17,7 @@ class Student < ActiveRecord::Base
   def self.search(keyword)
     @students = []
     Student.all.each do |s|
-      if s.name.include?(keyword)
+      if s.name.scan(/\w+/).include?(keyword)
         @students << s
       end
     end
